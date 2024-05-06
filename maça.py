725af166d373c8a2ad7618b389f3b15386291829
em11062007 <129952338+em11062007@users.noreply.github.com>
2024-05-06 08:48:57 -0300

    Update maça.py

--- maça.py
+++ maça.py
@@ -1,1 +1,13 @@
+numero_macas = int(input("Digite o número de maçãs compradas: "))
+
+
+if numero_macas < 12:
+    preco_unitario = 0.30
+else:
+    preco_unitario = 0.25
+
+
+valor_total = numero_macas * preco_unitario
+
+print(f"O valor total da compra é de R$ {valor_total:.2f}")
 
