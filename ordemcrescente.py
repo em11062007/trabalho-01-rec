1538ab6a18f7e2420bb43c9bfb08ea24c5965f58
em11062007 <129952338+em11062007@users.noreply.github.com>
2024-05-06 08:51:36 -0300

    Update ordemcrescente.py

--- ordemcrescente.py
+++ ordemcrescente.py
@@ -1,1 +1,10 @@
-# trabalho-01-rec
+num1 = int(input("Digite o primeiro valor inteiro: "))
+num2 = int(input("Digite o segundo valor inteiro: "))
+num3 = int(input("Digite o terceiro valor inteiro: "))
+
+
+valores = [num1, num2, num3]
+valores.sort()
+
+
+print("Valores em ordem crescente:", valores)
